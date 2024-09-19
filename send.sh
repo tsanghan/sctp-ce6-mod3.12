f174a66554b87c6e9877f91aad60001f07565fc2
Wong Tsang Han <tsanghan@gmail.com>
2024-09-20 00:00:15 +0800

    15th commit

---
+++ send.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for first_word in tartuffes unfunded pyramided gribbles unideaed sociopaths greening pulsated circumferences ; do
+  for second_ford in occupancies ulans grabby condores stupendousness respirometers brasseries logomachies hetero ecocide; do
+    curl -s -X POST https://japuswxev0.execute-api.ap-southeast-1.amazonaws.com/ --data-raw '{"'"$first_word"'":"'"$second_word"'"}'
+  done
+done
