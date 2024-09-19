1ca6d850c8762933871545db1873a3e4efed8e81
Wong Tsang Han <tsanghan@gmail.com>
2024-09-20 00:35:56 +0800

    16th commit

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for first_word in tartuffes unfunded pyramided gribbles unideaed sociopaths greening pulsated circumferences ; do
-  for second_ford in occupancies ulans grabby condores stupendousness respirometers brasseries logomachies hetero ecocide; do
-    curl -s -X POST https://japuswxev0.execute-api.ap-southeast-1.amazonaws.com/ --data-raw '{"'"$first_word"'":"'"$second_word"'"}'
+  for second_word in occupancies ulans grabby condores stupendousness respirometers brasseries logomachies hetero ecocide; do
+    curl -s -X POST $(aws apigatewayv2 get-apis | yq '.Items[] | select(.Name == "dev-tsanghan-ce6-dynamodb-sqs-service") | .ApiEndpoint')/ --data-raw '{"'"$first_word"'":"'"$second_word"'"}'
   done
 done
